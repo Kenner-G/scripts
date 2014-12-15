5e0c477809e261d3fd6bfe21330b8dbe93d3cc28
Piju <9m2pju@gmail.com>
2014-12-16 01:11:09 +0800

    Create aprx.sh

---
+++ aprx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#       script untuk update uptime dan temperature kepada satu baris tulisan
+#       untuk dibaca oleh aprx 2.7
+#
+
+#mula-mula delete existing files
+rm -rf beacon* && rm -rf temp* && rm -rf uptime*
+
+#lepas tu buat file temperature dengan uptime
+uptime > uptime.txt 
+#&& acpi -t > temp.txt
+
+#"allign" semua output temperature kepada single line
+#sed 'N;s/\n/ /' temp.txt > temp2.txt
+
+#masukkan nilai latlong, uptime dan temperature kepada satu temporary beacon file
+cat /root/latlong.txt >> /root/beacon.txt && cat uptime.txt >> /root/beacon.txt 
+#&& cat temp2.txt >> /root/beacon.txt
+
+#allignment nilai uptime kepada satu line
+awk 'NR%2{printf $0" ";next;}1' /root/beacon.txt > /root/beacon2.txt
+
+#allignment nilai temperature kepada satu line
+#sed 'N;s/\n/ /' beacon2.txt > beacon3.txt
+
+#/usr/bin/perl -lape 's/\s+//sg' output2 > output3
+#sed 's/up/ up/g' /root/output2 > /root/output3
+#sed 's/,/ /g' /root/output3 > /root/output4
